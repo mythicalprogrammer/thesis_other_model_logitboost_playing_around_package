7c3b94d35c085649bf77309a8cc72597592ccb47
Anthony Doan <mythicalprogrammer@gmail.com>
2019-02-04 18:53:04 -0800

    Added data, code to read data, and made a model to see how fast it takes to train.

---
+++ 10_testing.R
@@ -0,0 +1,14 @@
+prostate_data <- read.table("start_data/GDS1390_after_anova.csv",
+                            sep = ",",
+                            header = TRUE)
+
+rand_seed <- 1030
+set.seed(rand_seed)
+
+xlearn <- prostate_data
+xlearn$state <- NULL
+ylearn <- prostate_data$state
+
+fit1 <- LogitBoost(xlearn, ylearn, nIter = ncol(xlearn))
+
+
